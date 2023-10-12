2075a1a7ac55f8449fd78d680646d23699172af5
Ozuma0508 <41827547+Ozuma0508@users.noreply.github.com>
2023-10-12 13:24:22 +0900

    Update script.js

--- script.js
+++ script.js
@@ -202,7 +202,7 @@ function createMapInfo(mapNames) {
 	}
 }
 
-//消してもいいよ
+//消してもいいよ↓
 // 画像をクリックしたときのイベントリスナーを追加
 const imageElement = document.getElementById('rotateCameraButton');
 imageElement.addEventListener('click', rotateCamera);
@@ -216,7 +216,7 @@ function rotateCamera() {
     cameras[cameraMode].position.z = radius * Math.cos(theta);
     cameras[cameraMode].lookAt(new THREE.Vector3(0, 0, 0));
 }
-//ここまで
+//ここまで↑
 
 // 前回ハイライトされたオブジェクトを参照するための変数
 let highlightedObject = null;
