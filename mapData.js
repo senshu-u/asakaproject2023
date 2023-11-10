8fcfa46c1c08c5e0a502a677a0267ebc3de48be0
senshu-u <41827547+senshu-u@users.noreply.github.com>
2023-11-10 11:53:05 +0900

    Update mapData.js

--- mapData.js
+++ mapData.js
@@ -1,12 +1,86 @@
 export class MapData {
 	mapPath = "data/map3DModel/";
 	building10MapPath = this.mapPath + "building10/";
+	building3MapPath = this.mapPath + "building3/";
+	building1MapPath = this.mapPath + "building1/";
 
 	maps = {
 		"wholeMap.glb": {
 			"dirPath": this.mapPath,
 			"mapName": "全体マップ",
 			"floor": "",
+			"rooms": []
+		},
+		"1_B1.glb": {
+			"dirPath": this.building1MapPath,
+			"mapName": "1号館",
+			"floor": "B1",
+			"rooms": []
+		},
+		"1_1.glb": {
+			"dirPath": this.building1MapPath,
+			"mapName": "1号館",
+			"floor": "1",
+			"rooms": []
+		},
+		"1_2.glb": {
+			"dirPath": this.building1MapPath,
+			"mapName": "1号館",
+			"floor": "2",
+			"rooms": []
+		},
+		"1_3.glb": {
+			"dirPath": this.building1MapPath,
+			"mapName": "1号館",
+			"floor": "3",
+			"rooms": []
+		},
+		"1_4.glb": {
+			"dirPath": this.building1MapPath,
+			"mapName": "1号館",
+			"floor": "4",
+			"rooms": []
+		},
+		"3_1.glb": {
+			"dirPath": this.building3MapPath,
+			"mapName": "3号館",
+			"floor": "1",
+			"rooms": []
+		},
+		"3_2.glb": {
+			"dirPath": this.building3MapPath,
+			"mapName": "3号館",
+			"floor": "2",
+			"rooms": []
+		},
+		"3_3.glb": {
+			"dirPath": this.building3MapPath,
+			"mapName": "3号館",
+			"floor": "3",
+			"rooms": []
+		},
+		"3_4.glb": {
+			"dirPath": this.building3MapPath,
+			"mapName": "3号館",
+			"floor": "4",
+			"rooms": []
+		},
+		"3_5.glb": {
+			"dirPath": this.building3MapPath,
+			"mapName": "3号館",
+			"floor": "5",
+			"rooms": []
+		},
+		"3_6.glb": {
+			"dirPath": this.building3MapPath,
+			"mapName": "3号館",
+			"floor": "6",
+			"rooms": []
+		},
+		"3_7.glb": {
+			"dirPath": this.building3MapPath,
+			"mapName": "3号館",
+			"floor": "7",
 			"rooms": []
 		},
 		"10_1.glb": {
