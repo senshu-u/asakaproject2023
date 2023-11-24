fb4006b01da424cd19928c4c2423c09841cca113
senshu-u <41827547+senshu-u@users.noreply.github.com>
2023-11-24 10:51:27 +0900

    Update mapInfoIcon.js

--- mapInfoIcon.js
+++ mapInfoIcon.js
@@ -42,11 +42,11 @@ export class MapInfoIcon {
 			"shouldShowMapObjName": false,
 			"filterItem": "自販機"
 		},
-		"情報コアゾーン": {
-			"fileName": "icc.png",
-			"shouldShowMapObjName": false,
-			"filterItem": null
-		},
+		//"情報コアゾーン": {
+		//	"fileName": "icc.png",
+		//	"shouldShowMapObjName": true,
+		//	"filterItem": null
+		//},
 		"ソフトクリーム屋": {
 			"fileName": "ice.png",
 			"shouldShowMapObjName": false,
@@ -62,20 +62,65 @@ export class MapInfoIcon {
 			"shouldShowMapObjName": false,
 			"filterItem": null
 		},
-		"喫煙所": {
-			"fileName": "Smoking_area.png",
-			"shouldShowMapObjName": false,
-			"filterItem": "喫煙所"
-		},
 		"休憩所": {
 			"fileName": "restarea.png",
-			"shouldShowMapObjName": false,
+			"shouldShowMapObjName": true,
+			"filterItem": null
+		},
+		"アカデミーモール1": {
+			"fileName": "restarea.png",
+			"shouldShowMapObjName": true,
+			"filterItem": null
+		},
+		"アカデミーモール2": {
+			"fileName": "restarea.png",
+			"shouldShowMapObjName": true,
+			"filterItem": null
+		},
+		"アカデミーモール3": {
+			"fileName": "restarea.png",
+			"shouldShowMapObjName": true,
+			"filterItem": null
+		},
+		"アカデミーモール4": {
+			"fileName": "restarea.png",
+			"shouldShowMapObjName": true,
 			"filterItem": null
 		},
 		"モスバーガー": {
 			"fileName": "mos.png",
 			"shouldShowMapObjName": false,
 			"filterItem": null
+		},
+		"喫煙所": {
+			"fileName": "Smoking_area.png",
+			"shouldShowMapObjName": false,
+			"filterItem": "喫煙所"
+		},
+		"図書館": {
+			"fileName": "library.png",
+			"shouldShowMapObjName": false,
+			"filterItem": "図書館"
+		},
+		"教務課": {
+			"fileName": "office.png",
+			"shouldShowMapObjName": false,
+			"filterItem": "教務課"
+		},
+		"喫煙所": {
+			"fileName": "Smoking_area.png",
+			"shouldShowMapObjName": false,
+			"filterItem": "喫煙所"
+		},
+		"学生生活課": {
+			"fileName": "office.png",
+			"shouldShowMapObjName": false,
+			"filterItem": "喫煙所"
+		},
+		"保健室": {
+			"fileName": "office.png",
+			"shouldShowMapObjName": false,
+			"filterItem": "保健室"
 		},
 		"駐輪場": {
 			"fileName": "bicycleparkinglot.png",
@@ -94,13 +139,36 @@ export class MapInfoIcon {
 			{"fileName": "sharedtoilet.png", "filterItem": "トイレ"},
 			{"fileName": "printer.png", "filterItem": "プリンター"}
 		],
+		"2号館": [
+			{"fileName": "sharedtoilet.png", "filterItem": "トイレ"},
+			{"fileName": "printer.png", "filterItem": "プリンター"}
+		],
 		"3号館": [
+			{"fileName": "sharedtoilet.png", "filterItem": "トイレ"},
+		],
+		"4号館": [
+			{"fileName": "sharedtoilet.png", "filterItem": "トイレ"},
+		],
+		"5号館": [
+			{"fileName": "sharedtoilet.png", "filterItem": "トイレ"},
+		],
+		"7号館": [
+			{"fileName": "sharedtoilet.png", "filterItem": "トイレ"},
+		],
+		"8号館": [
 			{"fileName": "sharedtoilet.png", "filterItem": "トイレ"},
 		],
 		"10号館": [
 			{"fileName": "sharedtoilet.png", "filterItem": "トイレ"},
 			{"fileName": "restaurant.png", "filterItem": "食堂"},
-			{"fileName": "printer.png", "filterItem": "プリンター"}
+			{"fileName": "printer.png", "filterItem": "プリンター"},
+			{"fileName": "Smoking_area.png", "filterItem": "喫煙所"}
 		],
+		"グリーントップ": [
+			{"fileName": "sharedtoilet.png", "filterItem": "トイレ"},
+		],
+		"図書館別館": [
+			{"fileName": "sharedtoilet.png", "filterItem": "トイレ"},
+		]
 	}
 }
